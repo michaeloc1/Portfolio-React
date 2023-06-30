811ac18878a9f5c89935e3bf40712ecf20bf376b
michaeloc1 <michaeloc@verizon.net>
2023-06-30 10:36:41 -0400

    start customizing portfolio

---
+++ src/data/headerData.js
@@ -0,0 +1,11 @@
+import resume from '../assets/pdf/resume.pdf'
+import portfolioPic from '../assets/images/portfolio-pic-modified.jpg'
+
+export const headerData = {
+    name: 'Michael O\'Cone',
+    title: "Web Developer",
+    desciption:"As a software developer, I've always been fascinated by the intricate dance between logic and creativity that takes place within the realms of coding. It's a remarkable fusion of art and science, where lines of code come alive and weave together to create powerful, transformative experiences. ",
+   // image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+   image: portfolioPic,
+    resumePdf: resume
+}
