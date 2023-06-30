2db574dfbbe590636c2e1880cd862d6c6a55dc44
michaeloc1 <michaeloc@verizon.net>
2023-06-30 13:08:22 -0400

    Added experience

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,22 +1,22 @@
 export const experienceData = [
     {
         id: 1,
-        company: 'Globex Corporation',
-        jobtitle: 'Frontend Developer',
-        startYear: '2018',
-        endYear: '2019'
+        company: 'Bernard Hodes Group',
+        jobtitle: 'Software Developer',
+        startYear: '2000',
+        endYear: '2003'
     },
     {
         id: 2,
-        company: 'Vehement Capital Partners',
-        jobtitle: 'Backend Developer',
-        startYear: '2019',
+        company: 'Express Lawn Care',
+        jobtitle: 'Owner Operator',
+        startYear: '2003',
         endYear: '2020'
     },
     {
         id: 3,
-        company: 'Wonka Industries',
-        jobtitle: 'Marketing',
+        company: 'Home Depot',
+        jobtitle: 'Sales Associate',
         startYear: '2020',
         endYear: 'Present'
     },
