838fe3c48f94c541fef7364d82aa1e1acd059fc5
michaeloc1 <michaeloc@verizon.net>
2023-06-30 11:12:30 -0400

    Added about me

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Jane. I'm a web designer and developer based in Southampton, UK.",
-    description2: "During the day I work as a lead developer at a local agency and in the evening I  work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.",
+    description1: "My name is Michael O'Cone and I am a web developer building full stack web applications.",
+    description2: "My interest in computers started at an early age when I received a Commodore 64. I enjoyed experimenting with programming and observing real-time output.  I also like building PC's and have built several already.",
     image: 2
 }
