38fbfb25508bf6859e033850a75ada9c17dbca70
michaeloc1 <michaeloc@verizon.net>
2023-07-01 10:30:48 -0400

    Added achievments

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -1,30 +1,33 @@
+import microsoftpic from '../assets/images/98785_new_microsoft_logo_icon.png'
+
 export const achievementData = {
-    bio : "Putting quantified achievements on a resume is great, but I just don’t work with hard numbers. I have no data to support the quality of my work. If you have lots of relevant experience, the heading statement of your resume will be a summary.",
+    //bio : "Putting quantified achievements on a resume is great, but I just don’t work with hard numbers. I have no data to support the quality of my work. If you have lots of relevant experience, the heading statement of your resume will be a summary.",
+    bio: '',
     achievements : [
         {
             id : 1,
-            title : 'Google Cloud certification',
-            details : 'Google Cloud certifications validate your expertise and showcase your ability to transform businesses with Google Cloud technology.',
-            date : 'Aug 20, 2019',
-            field : 'Automation',
-            image : 'https://images.unsplash.com/photo-1573141597928-403fcee0e056?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80'
+            title : 'Rutgers Coding Certificate',
+            details : 'Participants have gained proficiency in various programming languages, web development, database management, and other in-demand tech skills.',
+            date : 'June 12, 2023',
+            field : 'Web Development',
+            image : 'https://api.accredible.com/v1/frontend/credential_website_embed_image/badge/76733002'
         },
         {
             id : 2,
-            title : 'AWS Certification',
-            details : 'Validate technical skills and cloud expertise to grow your career and business.',
-            date : 'Aug 20, 2019',
-            field : 'Automation',
-            image : 'https://images.unsplash.com/photo-1523474253046-8cd2748b5fd2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+            title : 'Microsoft Certified Solution Developer (MCSD)',
+            details : 'Successfully passed Microsoft’s exam in designing and development of business applications using Microsoft’s toolkits and technologies.',
+            date : 'Nov 20, 2000',
+            field : 'Software Development',
+            image : microsoftpic
         },
-        {
-            id : 3,
-            title : 'Professional Data Engineer Certification',
-            details : 'Professional Data Engineers enable data-driven decision making by collecting, transforming, and publishing data.',
-            date : 'Aug 20, 2019',
-            field : 'Automation',
-            image : 'https://images.unsplash.com/photo-1489389944381-3471b5b30f04?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
-        }
+        // {
+        //     id : 3,
+        //     title : 'Professional Data Engineer Certification',
+        //     details : 'Professional Data Engineers enable data-driven decision making by collecting, transforming, and publishing data.',
+        //     date : 'Aug 20, 2019',
+        //     field : 'Automation',
+        //     image : 'https://images.unsplash.com/photo-1489389944381-3471b5b30f04?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+        // }
 
     ]
 }
